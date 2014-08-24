814527adb45bbcf9c841c8b8084974c31f1e8ae8
yui-knk <spiketeika@gmail.com>
2014-08-24 13:20:22 +0900

    Add spec

---
+++ spec/pry-power_assert_spec.rb
@@ -0,0 +1,43 @@
+require 'pry/test/helper'
+require 'pry-power_assert'
+
+describe "ps command" do
+  it "should show power_assert result" do
+    pry_eval(%q{pa "0".class == "3".to_i.times.map {|i| i + 1 }.class}).chomp.should == <<END.chomp
+"0".class == "3".to_i.times.map {|i| i + 1 }.class
+    |     |      |    |     |                |
+    |     |      |    |     |                Array
+    |     |      |    |     [1, 2, 3]
+    |     |      |    #<Enumerator: 3:times>
+    |     |      3
+    |     false
+    String
+END
+  end
+
+  it "should show power_assert result" do
+    pry_eval(%q{a = 10})
+    pry_eval(%q{b = 20})
+    pry_eval(%q{pa a + b + 30}).chomp.should == <<END.chomp
+a + b + 30
+| | | |
+| | | 60
+| | 20
+| 30
+10
+END
+  end
+
+  it "should show power_assert result" do
+    pry_eval(%q{@a = 10})
+    pry_eval(%q{@b = 20})
+    pry_eval(%q{pa @a + @b + 30}).chomp.should == <<END.chomp
+@a + @b + 30
+|  | |  |
+|  | |  60
+|  | 20
+|  30
+10
+END
+  end
+end
